3056d795c50a369ae20c61353d1ee3fe7a8ac60e
Alexandre Blondeau <deformhead@gmail.com>
2018-12-08 17:46:21 +0100

    update version

--- generators/app/templates/sources/theatre/core/theatre.js
+++ generators/app/templates/sources/theatre/core/theatre.js
@@ -111,7 +111,7 @@ function Theatre(config) {
     this.scenes = scenes;
     this.size = size;
     this.state = {};
-    this.version = '0.11.0';
+    this.version = '0.12.0';
 
     this.load = load;
     this.restart = restart;
