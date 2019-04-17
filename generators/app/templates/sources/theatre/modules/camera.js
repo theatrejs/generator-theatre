1b7309d474dd96e99b02a7756f6ddf13d375450a
Alexandre Blondeau <deformhead@gmail.com>
2019-04-17 20:45:25 +0200

    update 'camera' module

--- generators/app/templates/sources/theatre/modules/camera.js
+++ generators/app/templates/sources/theatre/modules/camera.js
@@ -5,6 +5,24 @@ function Camera(context, width, height) {
 
     let images = [];
 
+    let shaking = {
+
+        'current': false,
+        'duration': 0,
+        'easing': null,
+        'elapsed': 0,
+        'force': {
+
+            'x': 0,
+            'y': 0
+        },
+        'shift': {
+
+            'x': 0,
+            'y': 0
+        }
+    };
+
     function add(image) {
 
         let target = 0;
@@ -31,7 +49,7 @@ function Camera(context, width, height) {
             const {frame, position, source} = image;
 
             const imageBox = new Rectangle(position.x, position.y, frame.width, frame.height);
-            const cameraBox = new Rectangle(this.position.x, this.position.y, this.size.width, this.size.height);
+            const cameraBox = new Rectangle(this.position.x + shaking.shift.x, this.position.y + shaking.shift.y, this.size.width, this.size.height);
 
             if (collide(imageBox, cameraBox) === true) {
 
@@ -39,7 +57,7 @@ function Camera(context, width, height) {
 
                     source,
                     frame.x, frame.y, frame.width, frame.height,
-                    position.x - this.position.x, position.y - this.position.y, frame.width, frame.height
+                    position.x - (this.position.x + shaking.shift.x), position.y - (this.position.y + shaking.shift.y), frame.width, frame.height
                 );
             }
         });
@@ -59,6 +77,60 @@ function Camera(context, width, height) {
         this.position.y = y;
     }
 
+    function shake(force, duration, easing) {
+
+        const {x, y} = force;
+
+        shaking.force = {
+
+            'x': x,
+            'y': y
+        };
+
+        shaking.current = true;
+        shaking.duration = duration;
+        shaking.easing = easing;
+    }
+
+    function update(delta) {
+
+        if (shaking.current === false) {
+
+            return;
+        }
+
+        if (shaking.elapsed + delta >= shaking.duration) {
+
+            shaking = {
+
+                'current': false,
+                'duration': 0,
+                'easing': null,
+                'elapsed': 0,
+                'force': {
+
+                    'x': 0,
+                    'y': 0
+                },
+                'shift': {
+
+                    'x': 0,
+                    'y': 0
+                }
+            };
+
+            return;
+        }
+
+        shaking.elapsed += delta;
+
+        const amplitude = shaking.easing(shaking.elapsed / shaking.duration);
+        const angle = Math.random() * 2 * Math.PI;
+
+        shaking.shift.x = Math.round(Math.cos(angle) * shaking.force.x * amplitude);
+        shaking.shift.y = Math.round(Math.sin(angle) * shaking.force.y * amplitude);
+    }
+
     this.position = {
 
         'x': 0,
@@ -75,6 +147,8 @@ function Camera(context, width, height) {
     this.draw = draw;
     this.move = move;
     this.set = set;
+    this.shake = shake;
+    this.update = update;
 }
 
 // exports current module as an object
