85401fdb2f02c53c863b5b72c05c3b4248cbeffa
Alexandre Blondeau <deformhead@gmail.com>
2019-05-04 14:21:28 +0200

    update 'ecs' generator

---
+++ generators/ecs/templates/sources/game/components/common/forces.js
@@ -0,0 +1,37 @@
+import * as Ease from 'modules/ease.js';
+
+function Forces(parts) {
+
+    this.name = 'forces';
+
+    function add(force) {
+
+        const {easing, elapsed, ending, duration, x, y} = force;
+
+        force.x = x;
+        force.y = y;
+        force.duration = duration;
+        force.easing = easing || Ease.linear(1);
+        force.elapsed = elapsed || 0;
+        force.ending = ending || false;
+
+        force.moved = {
+
+            'x': 0,
+            'y': 0
+        };
+
+        this.parts.push(force);
+    }
+
+    this.parts = [];
+
+    this.add = add;
+
+    parts.forEach((force) => {
+
+        this.add(force);
+    });
+}
+
+export {Forces};
---
+++ generators/ecs/templates/sources/game/systems/common/forces.js
@@ -0,0 +1,43 @@
+function forces(entity) {
+
+    const forcesComponent = entity.get('forces');
+    const positionComponent = entity.get('position');
+
+    forcesComponent.parts.forEach((force) => {
+
+        const unlimited = force.ending === false;
+        const remaining = force.duration - force.elapsed;
+        const delta = (unlimited === false && this.delta.update > remaining) ? remaining : this.delta.update;
+
+        const progress = (force.elapsed + delta) / force.duration;
+
+        const moved = {
+
+            'x': force.x * force.easing(progress),
+            'y': force.y * force.easing(progress)
+        };
+
+        positionComponent.x += moved.x - force.moved.x;
+        positionComponent.y += moved.y - force.moved.y;
+        force.moved = moved;
+
+        force.elapsed += this.delta.update;
+    });
+
+    forcesComponent.parts.forEach((force) => {
+
+        if (force.elapsed >= force.duration
+        && typeof force.ending === 'function') {
+
+            force.ending(entity, force.elapsed - force.duration);
+        }
+    });
+
+    forcesComponent.parts = forcesComponent.parts.filter((force) => {
+
+        return (force.elapsed <= force.duration
+        || force.ending === false);
+    });
+}
+
+export {forces};
