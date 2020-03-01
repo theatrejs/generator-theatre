ee61a1dda1d371a36afd0aea3a2ebb75c3116847
Alexandre Blondeau <deformhead@gmail.com>
2020-03-01 11:54:46 +0100

    add 'entities' folder

--- generators/app/templates/webpack.common.js
+++ generators/app/templates/webpack.common.js
@@ -46,6 +46,7 @@ module.exports = {
 
             'assets': path.resolve(__dirname, 'sources/game/assets/'),
             'components': path.resolve(__dirname, 'sources/game/components/'),
+            'entities': path.resolve(__dirname, 'sources/game/entities/'),
             'scenes': path.resolve(__dirname, 'sources/game/scenes/'),
             'systems': path.resolve(__dirname, 'sources/game/systems/'),
 
