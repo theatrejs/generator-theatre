2ef763300ca37ea77a41676fd8be1b0389e75dfa
Alexandre Blondeau <deformhead@gmail.com>
2020-03-25 09:12:00 +0100

    update loop

--- generators/app/templates/sources/theatre/core/loop.js
+++ generators/app/templates/sources/theatre/core/loop.js
@@ -6,7 +6,7 @@ function Loop(framerate = 60, speed = 1) {
 
     function render(handler) {
 
-        const currentRender = Date.now();
+        const currentRender = performance.now();
 
         if (lastRender !== null) {
 
@@ -21,27 +21,27 @@ function Loop(framerate = 60, speed = 1) {
 
     function update(handler) {
 
-        const currentUpdate = Date.now();
+        const currentUpdate = performance.now();
 
         if (lastUpdate !== null) {
 
             // define elapsed time since last update
             elapsedTime += currentUpdate - lastUpdate;
+
+            // call user's update handler matching timeframe, speed and fixing browser time handling
+            while (elapsedTime >= 1000 / this.framerate / this.speed) {
+
+                // define elapsed time since last user's update handler matching timeframe and speed
+                elapsedTime -= 1000 / this.framerate / this.speed;
+
+                handler(1000 / this.framerate);
+            }
         }
 
-        // call user's update handler matching timeframe, speed and fixing browser time handling
-        while (elapsedTime >= 1000 / this.framerate / this.speed) {
-
-            // define elapsed time since last user's update handler matching timeframe and speed
-            elapsedTime -= 1000 / this.framerate / this.speed;
-
-            handler(1000 / this.framerate);
-        }
+        // call user's update handler on each available frame
+        requestAnimationFrame(this.update.bind(this, handler));
 
         lastUpdate = currentUpdate;
-
-        // call update loop at least 60 times per second to quickly catch framerate or speed changes
-        setTimeout(this.update.bind(this, handler), 1000 / Math.max(this.framerate, 60));
     }
 
     this.framerate = framerate;
