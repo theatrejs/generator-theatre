999b31b01f16edfabd129d5e2838392a8bb54671
Alexandre Blondeau <deformhead@gmail.com>
2020-04-07 19:03:19 +0200

    update 'app' generator

--- generators/app/templates/sources/game/scenes/demo/update.js
+++ generators/app/templates/sources/game/scenes/demo/update.js
@@ -1,5 +1,6 @@
 import {forces} from 'systems/common/forces.js';
 import {inputs} from 'systems/common/inputs.js';
+import {timeout} from 'systems/common/timeout.js';
 
 import {commands} from 'systems/demo/commands.js';
 
@@ -7,9 +8,10 @@ function update() {
 
     // console.log('update demo scene');
 
-    this.$world.system(['inputs'], inputs);
-    this.$world.system(['commands'], commands);
-    this.$world.system(['position', 'forces'], forces);
+    this.$world.system('inputs', ['inputs'], inputs);
+    this.$world.system('commands', ['commands'], commands);
+    this.$world.system('timeout', ['timeout'], timeout);
+    this.$world.system('forces', ['position', 'forces'], forces);
 
     this.$camera.update(this.delta.update);
 }
