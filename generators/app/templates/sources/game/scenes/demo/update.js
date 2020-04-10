78d43a19c7c3182b7ad85c7d309cb6c6ccd97e40
Alexandre Blondeau <deformhead@gmail.com>
2020-04-10 20:25:43 +0200

    update 'demo' scene

--- generators/app/templates/sources/game/scenes/demo/update.js
+++ generators/app/templates/sources/game/scenes/demo/update.js
@@ -1,4 +1,6 @@
+import {fade} from 'systems/common/fade.js';
 import {forces} from 'systems/common/forces.js';
+import {spritesheets} from 'systems/common/spritesheets.js';
 import {timeout} from 'systems/common/timeout.js';
 
 import {commands} from 'systems/demo/commands.js';
@@ -10,6 +12,10 @@ function update() {
     this.$world.system('timeout', ['timeout'], timeout);
     this.$world.system('commands', ['commands'], commands);
     this.$world.system('forces', ['position', 'forces'], forces);
+    this.$world.system('fade', ['camera', 'fade'], fade);
+    this.$world.system('spritesheets', ['images'], spritesheets);
+
+    this.$camera.update(this.delta.update);
 }
 
 export {update};
