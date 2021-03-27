61b95bbcd6451ac62aae4854d9e22790028c43f2
Alexandre Blondeau <deformhead@gmail.com>
2021-03-27 20:42:14 +0100

    update 'demo' scene

--- generators/app/templates/sources/game/scenes/demo/render.js
+++ generators/app/templates/sources/game/scenes/demo/render.js
@@ -2,7 +2,7 @@ function render() {
 
     // console.log('lifecycle :', 'render demo scene');
 
-    this.context.fillStyle = '#cbdbfc';
+    this.context.fillStyle = '#000000';
     this.context.fillRect(0, 0, this.size.width, this.size.height);
 
     this.$.world.system('images', ['cameras', 'images', 'position'], this.systems.common.images);
