af4381f4772567fe10114118313e19e22596acb1
Alexandre Blondeau <deformhead@gmail.com>
2021-04-30 19:31:30 +0200

    remove 'state' system

--- generators/app/templates/sources/game/snippets/common/handle-state.js
+++
@@ -1,7 +0,0 @@
-export default function (entity, changes) {
-
-    console.log('debugging :', 'component', '\'' + 'state' + '\'');
-    console.log(changes);
-    console.log('debugging :', 'entity', '\'' + entity.name + '\'');
-    console.log(entity);
-};
